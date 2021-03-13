7d73f8a2dbd38dbe9378b8dc38fe8e55e8609204
kaustubh <kaustubh.khulbe@gmail.com>
2021-03-12 16:28:45 -0800

    switched to typejs instead of typeit

--- main.js
+++ main.js
@@ -13,17 +13,18 @@ const scroll = new SmoothScroll('.navbar a[href*="#"]', {
      speed: 600
 });
 
-new TypeIt("#home", {
-     speed: 100,
-     loop: false
-   }, function(){
-     // callback
-     $('.ti-cursor').remove();
-   }).go();
-
+var typed = new Typed('.home_des', {
+  strings: ['I am glad you came!', 'I hope you learn something about me!'],
+  showCursor: false,
+  typeSpeed: 100,
+  backspeed: 10,
+  backDelay: 2000,
+  loop: true
+});
 
 
   
-  const about_text = "I am a passionate Robotics and Computer Science student. I am hardworking and will spend time self learning any topics. I have been an extremely active member in FRC Team 846, The Funky Monkeys. In the team, I have worked on both software and design. In addition, I have worked on my own to develop several projects. I primarily use Java, Python, and Kotlin to develop projects."
-  document.getElementById("about_paragraph").textContent += about_text
+const about_text = "I am a passionate Robotics and Computer Science student. I am hardworking and will spend time self learning any topics. I have been an extremely active member in FRC Team 846, The Funky Monkeys. In the team, I have worked on both software and design. In addition, I have worked on my own to develop several projects. I primarily use Java, Python, and Kotlin to develop projects."
+document.getElementById("about_paragraph").textContent += about_text
+
   
